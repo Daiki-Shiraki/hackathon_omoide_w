85a949cbfabb58a24aaabf007ce3bf805bf773b6
secchanu <secchanu929@gmail.com>
2022-09-15 16:20:26 +0900

    add env

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import app from "./app";
 import api from "./api";
 
-const port = 3000;
+const port = process.env["PORT"] || 3000;
 const server = express();
 
 server.use("/", app);
