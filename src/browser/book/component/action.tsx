fd1f5b3b36e1087fc9c84d91b3f6566150e875bb
secchanu <secchanu929@gmail.com>
2022-09-15 11:12:16 +0900

    雛形

---
+++ src/browser/book/component/action.tsx
@@ -0,0 +1,9 @@
+import type { FunctionComponent } from "react";
+
+//import styles from "../css/name.module.css";
+
+const Component: FunctionComponent = () => {
+  return <div></div>;
+};
+
+export default Component;
--- src/browser/book/index.tsx
+++ src/browser/book/index.tsx
@@ -1,9 +1,24 @@
 import type { FunctionComponent } from "react";
+import Action from "./component/action";
+import Canvas from "./component/canvas";
+import Sight from "./component/sight";
 
 import styles from "./css/style.module.css";
 
 const Component: FunctionComponent = () => {
-  return <div className={styles.container}>book</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <Sight />
+      </div>
+      <div>
+        <Canvas />
+      </div>
+      <div>
+        <Action />
+      </div>
+    </div>
+  );
 };
 
 export default Component;
