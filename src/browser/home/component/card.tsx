17d09bb1431d156670d217d8eec2987d9535507d
secchanu <secchanu929@gmail.com>
2022-09-16 13:46:27 +0900

    add link

--- src/browser/home/component/card.tsx
+++ src/browser/home/component/card.tsx
@@ -11,10 +11,15 @@ const Component: FunctionComponent<Props> = (props) => {
   const name = props.data.name;
   const canvas = props.data.canvas;
   const odd = props.odd;
+  const sightId = props.data.sight_ID;
+
+  const redirectToBook = () => {
+    window.location.href = `./book?id=${sightId}`;
+  };
 
   return (
     <div className={odd ? styles.even : styles.odd}>
-      <div className={styles.card}>
+      <div className={styles.card} onClick={redirectToBook}>
         <img className={styles.ulb} src={canvas} />
         <div className={styles.name}>{name}</div>
       </div>
